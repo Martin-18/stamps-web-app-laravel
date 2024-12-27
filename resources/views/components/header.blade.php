fb39908ff3fb9cbd839543a69a609ce75d9d6174
Luis Eduardo Sinisterra Montaño <luiseduardosinisterramontano@gmail.com>
2024-12-27 17:08:03 -0500

    improve the main page4

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,4 +1,4 @@
-<header class="shadow-md font-semibold tracking-wide bg-[#fdfaf2] text-gray-700 relative z-50 sticky top-0">
+<header class="shadow-md font-semibold tracking-wide bg-gray-100 text-gray-700 relative z-50 sticky top-0">
     <section class="md:flex items-center py-4 lg:px-10 px-4 border-b border-gray-300 justify-between">
         <!-- Logo de la tienda -->
         <a href="/" class="max-sm:w-full shrink-0">
@@ -13,14 +13,18 @@
         </div>
 
         <!-- Buscador visible solo en dispositivos grandes -->
-        <div class="flex-grow mx-4 relative hidden md:block">
-            <input type="text" placeholder="Buscar estampillas..." 
-                class="w-full bg-gray-100 focus:bg-white px-4 py-2 rounded-full shadow-inner outline-none text-sm placeholder-gray-500 transition-all" />
-            <button class="absolute right-2 top-2 text-gray-600 hover:text-gray-800">
-                <!-- Icono de búsqueda -->
-                <i class="fa-solid fa-magnifying-glass"></i>
+        <div class="flex rounded-md border-2 border-[#3A4750] overflow-hidden max-w-lg mx-auto font-[sans-serif]">
+            <input type="email" placeholder="Buscar estampillas..."
+            class="w-[1600px] outline-none bg-white text-gray-600 text-sm px-4 py-3" />
+            <button type='button' class="flex items-center justify-center bg-[#3A4750] px-5">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 192.904 192.904" width="16px" class="fill-white">
+                <path
+                d="m190.707 180.101-47.078-47.077c11.702-14.072 18.752-32.142 18.752-51.831C162.381 36.423 125.959 0 81.191 0 36.422 0 0 36.423 0 81.193c0 44.767 36.422 81.187 81.191 81.187 19.688 0 37.759-7.049 51.831-18.751l47.079 47.078a7.474 7.474 0 0 0 5.303 2.197 7.498 7.498 0 0 0 5.303-12.803zM15 81.193C15 44.694 44.693 15 81.191 15c36.497 0 66.189 29.694 66.189 66.193 0 36.496-29.692 66.187-66.189 66.187C44.693 147.38 15 117.689 15 81.193z">
+                </path>
+            </svg>
             </button>
         </div>
+
         
         @if (Route::has('login'))
             <nav class="flex items-center space-x-6 hidden md:flex">
@@ -34,12 +38,12 @@
                 @else
 
                     <a href="{{ route('login') }}" class="text-gray-600 hover:text-gray-800 transition">
-                        <i class="fa-solid fa-right-to-bracket"></i> Iniciar sesión
+                        <i class="fa-solid fa-right-to-bracket"></i>
                     </a>
 
                     @if (Route::has('register'))
                         <a href="{{ route('register') }}" class="text-gray-600 hover:text-gray-800 transition">
-                            <i class="fa-solid fa-user-plus"></i> Registrarse
+                            <i class="fa-solid fa-user-plus"></i>
                         </a>
                     @endif
                 @endauth
