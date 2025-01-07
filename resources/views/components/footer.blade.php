6a97c16ac1d4733d4a0dd63e79b576ae54b91138
Luis Eduardo Sinisterra Montaño <luiseduardosinisterramontano@gmail.com>
2025-01-06 21:01:21 -0500

    new footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,11 +1,6 @@
-<footer class="bg-[#3A4750] text-gray-300 py-6 px-16 tracking-wide fixed sticky bottom-0 w-full">
-    <div class="flex justify-between items-center max-lg:flex-col text-center flex-wrap gap-4">
-    <p class="text-[15px] leading-loose">© Todos los derechos reservados 2024</p>
-
-    <ul class="flex space-x-6 gap-y-2 max-lg:justify-center flex-wrap">
-        <li><a href="javascript:void(0)" class="text-[15px] hover:text-white">Terminos y condiciones</a></li>
-        <li><a href="javascript:void(0)" class="text-[15px] hover:text-white">Poliza de privacidad</a></li>
-        <li><a href="javascript:void(0)" class="text-[15px] hover:text-white">Contacto</a></li>
-    </ul>
-    </div>
-</footer>      
+    <!-- Footer -->
+    <footer class="bg-gray-100 py-4">
+      <div class="container mx-auto text-center text-gray-600">
+        &copy; 2025 StampShop. All rights reserved.
+      </div>
+    </footer>
